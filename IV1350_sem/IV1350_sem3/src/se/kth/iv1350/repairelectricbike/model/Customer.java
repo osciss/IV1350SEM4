59363d9dadf8cb4df49412844122f214ae8ab034
Oscar Granath <osgr@kth.se>
2026-05-02 13:24:33 +0200

    Expand Customer model with bike and contact fields
    Replace previous Customer implementation to store more customer and bike details. Removed customerID, added email, bikeBrand, bikeSerialNo and bikeModel, kept name and phoneNumber. Updated constructor signature and added getters for the new fields; also added Javadoc for the constructor.

--- IV1350_sem/IV1350_sem3/src/se/kth/iv1350/repairelectricbike/model/Customer.java
+++ IV1350_sem/IV1350_sem3/src/se/kth/iv1350/repairelectricbike/model/Customer.java
@@ -1,24 +1,48 @@
 package se.kth.iv1350.repairelectricbike.model;
 
 public class Customer {
-    private int customerID;
-    private String name;
-    private String phoneNumber;
-
-    // Constructor for the Customer class.
-    public Customer(int customerID, String name, String phoneNumber) {
-        this.customerID = customerID;
+    private  String name;
+    private  String email;
+    private  String phoneNumber;
+    private  String bikeBrand;
+    private  String bikeSerialNo;
+    private  String bikeModel;
+/**
+ * Creates a new Customer.
+ *
+ * @param name The customer's name.
+ * @param email The customer's email.
+ * @param bikeBrand The bike's brand.
+ * @param bikeSerialNo The bike's serial number.
+ * @param bikeModel The bike's model.
+ */
+    public Customer(String name, String email, String phoneNumber, String bikeBrand, String bikeSerialNo, String bikeModel) {
         this.name = name;
+        this.email = email;
+        this.bikeBrand = bikeBrand;
+        this.bikeSerialNo = bikeSerialNo;
+        this.bikeModel = bikeModel;
         this.phoneNumber = phoneNumber;
-    }
-    // Getters for the Customer class.
-    public int getCustomerID() {
-        return customerID;
     }
     public String getName() {
         return name;
+    }   
+    public String getEmail() {
+        return email;
     }
     public String getPhoneNumber() {
         return phoneNumber;
     }
+    public String getBikeBrand() {
+        return bikeBrand;
+    }
+    public String getBikeSerialNo() {
+        return bikeSerialNo;
+    }
+    public String getBikeModel() {
+        return bikeModel;
+    }
+
+
+
 }
