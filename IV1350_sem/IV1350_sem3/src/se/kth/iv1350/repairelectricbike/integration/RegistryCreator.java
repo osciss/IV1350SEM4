9538eeb1725c1692aa7efc86c9ca180905d75c4b
Oscar Granath <osgr@kth.se>
2026-05-02 12:30:01 +0200

    Initialize registries with constructor and getters
    Replace incorrect constructor-like method with a proper RegistryCreator() constructor that initializes CustomerRegistry and RepairOrderRegistry. Add private fields, imports, Javadoc, and public getters for both registries to expose them to callers. Cleans up class structure and fixes object creation logic.

--- IV1350_sem/IV1350_sem3/src/se/kth/iv1350/repairelectricbike/integration/RegistryCreator.java
+++ IV1350_sem/IV1350_sem3/src/se/kth/iv1350/repairelectricbike/integration/RegistryCreator.java
@@ -1,11 +1,30 @@
 
 package se.kth.iv1350.repairelectricbike.integration;
+import se.kth.iv1350.repairelectricbike.integration.CustomerRegistry;
+import se.kth.iv1350.repairelectricbike.integration.RepairOrderRegistry;
+
+
 /// This class is responsible for creating the customer and repairorder registry and its dependencies.
+/**
+ * This class is responsible for creating the customer and repair order registry and its dependencies.
+ */
 public class RegistryCreator {
-   // Creates the registry and its dependencies.
-   public RegistryCreator RegistryCreator(){
-        RegistryCreator registryCreator = new RegistryCreator();
-        return registryCreator;   
+    private CustomerRegistry customerRegistry;
+    private RepairOrderRegistry repairOrderRegistry;
+
+    /**
+     * Creates the registry and its dependencies.
+     */
+    public RegistryCreator() {
+        customerRegistry = new CustomerRegistry();
+        repairOrderRegistry = new RepairOrderRegistry();  
     }
-    
+
+    public CustomerRegistry getCustomerRegistry() {
+        return customerRegistry;
+    }
+
+    public RepairOrderRegistry getRepairOrderRegistry() {
+        return repairOrderRegistry;
+    }
 }
