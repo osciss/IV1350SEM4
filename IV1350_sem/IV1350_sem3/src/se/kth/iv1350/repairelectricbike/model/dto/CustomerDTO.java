16fb2b3223be0d9985830328cc7d7e057703a031
Oscar Granath <osgr@kth.se>
2026-05-02 12:17:43 +0200

    Update CustomerDTO.java

--- IV1350_sem/IV1350_sem3/src/se/kth/iv1350/repairelectricbike/model/dto/CustomerDTO.java
+++ IV1350_sem/IV1350_sem3/src/se/kth/iv1350/repairelectricbike/model/dto/CustomerDTO.java
@@ -4,10 +4,12 @@ package se.kth.iv1350.repairelectricbike.model.dto;
  * Contains customer information and is used to transfer data between the layers.
  */
 public class CustomerDTO {
-    public final String name;
-    public final String email;
-    public final String bikeBrand;
-    public final String bikeSerialNo;
+    private final String name;
+    private final String email;
+    private final String phoneNumber;
+    private final String bikeBrand;
+    private final String bikeSerialNo;
+    private final String bikeModel;
 
 /**
  * Creates a new CustomerDTO.
@@ -16,11 +18,35 @@ public class CustomerDTO {
  * @param email The customer's email.
  * @param bikeBrand The bike's brand.
  * @param bikeSerialNo The bike's serial number.
+ * @param bikeModel The bike's model.
  */
-    public CustomerDTO(String name, String email, String bikeBrand, String bikeSerialNo) {
+    public CustomerDTO(String name, String email, String phoneNumber, String bikeBrand, String bikeSerialNo, String bikeModel) {
         this.name = name;
         this.email = email;
         this.bikeBrand = bikeBrand;
         this.bikeSerialNo = bikeSerialNo;
+        this.bikeModel = bikeModel;
+        this.phoneNumber = phoneNumber;
     }
+    public String getName() {
+        return name;
+    }   
+    public String getEmail() {
+        return email;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public String getBikeBrand() {
+        return bikeBrand;
+    }
+    public String getBikeSerialNo() {
+        return bikeSerialNo;
+    }
+    public String getBikeModel() {
+        return bikeModel;
+    }
+
+
+
 }
